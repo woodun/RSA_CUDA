6ff6e247d2dbe858fe42fe5af5d91c28bdf70a34
woodun <hwang07@email.wm.edu>
2018-11-05 05:01:24 -0500

    debug

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
-./main nodiv_65.txt 1branchcombo0000_65.txt 2branchcombo0000_65.txt 10
-./main div_65.txt 1branchcombo0000_65.txt 3branchcombo0100_65.txt 10
+./main nodiv_65.txt 1branchcombo0000_65.txt 2branchcombo0000_65.txt 100
+./main div_65.txt 1branchcombo0000_65.txt 3branchcombo0100_65.txt 100
 
-./main nodiv_64.txt 1branchcombo0000_64.txt 2branchcombo0000_64.txt 10
-./main div_64.txt 1branchcombo0000_64.txt 3branchcombo0100_64.txt 10
+./main nodiv_64.txt 1branchcombo0000_64.txt 2branchcombo0000_64.txt 100
+./main div_64.txt 1branchcombo0000_64.txt 3branchcombo0100_64.txt 100
