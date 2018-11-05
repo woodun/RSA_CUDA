403e56fe16e173547217630e46eb116b1468f59a
woodun <hwang07@email.wm.edu>
2018-11-05 18:52:45 -0500

    debug

--- launch.sh
+++ launch.sh
@@ -5,4 +5,8 @@
 #./main div_64.txt 1branchcombo0000_64.txt 3branchcombo0100_64.txt 100
 
 
-echo 10000 > asd.txt
+./main nodiv_65.txt 1branchcombo0000_1.txt 2branchcombo0000_1.txt 100
+./main div_65.txt 1branchcombo0000_1.txt 3branchcombo0100_1.txt 100
+
+./main nodiv_64.txt 1branchcombo0000_0.txt 2branchcombo0000_0.txt 100
+./main div_64.txt 1branchcombo0000_0.txt 3branchcombo0100_0.txt 100
