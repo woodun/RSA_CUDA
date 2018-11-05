b45fe9c13a50dafc3a199c451999134afb795392
woodun <hwang07@email.wm.edu>
2018-11-05 01:40:04 -0500

    debug

--- main.cu
+++ main.cu
@@ -102,12 +102,12 @@ int main (int argc, char *argv[]) {//./main nodiv.txt 1branchcombo0000.txt 2bran
 	///////get Message1
 	//char mes1_input[] = "00000000000123456789";
 	char mes1_input[] = "0000002a6975d3419d6adaaa";
-	mpz_set_str_host(&myMes1_h[0], mes1_input); //input from string
+	//mpz_set_str_host(&myMes1_h[0], mes1_input); //input from string
 
 	///////get Message2
 	//char mes2_input[] = "00000000000987654321";
 	char mes2_input[] = "00000008c6d8166335bef22e";
-	mpz_set_str_host(&myMes2_h[0], mes2_input); //input from string
+	//mpz_set_str_host(&myMes2_h[0], mes2_input); //input from string
 
 //	//debug
 //	char test_str[1024];
