1d277a8489709c1184f0fe5c779ca33c21954abf
woodun <hwang07@email.wm.edu>
2018-11-13 03:06:27 -0500

    debug

--- complete_recovery/kernel.cu
+++ complete_recovery/kernel.cu
@@ -3,104 +3,104 @@
 #include "cuda_mpz.h"
 
 
-//__device__ __host__ cuda_mpz_t* REDC(int RL, cuda_mpz_t* N, cuda_mpz_t* N_, cuda_mpz_t* T, cuda_mpz_t* tmp, cuda_mpz_t* t){//cuda_mpz_t* RMOD, int L, cuda_mpz_t* N, cuda_mpz_t* N_ should not be changed.
-//
-//	//m = ((T & R) * N_) & R
-//	cuda_mpz_bitwise_truncate(t, T, RL);
-//	cuda_mpz_mult(tmp, N_, t);
-//	cuda_mpz_bitwise_truncate_eq(tmp, RL);
-//
-//	//t = (T + m*N) >> L
-//	cuda_mpz_mult(t, tmp , N);
-//	cuda_mpz_add(tmp, T, t);
-//	cuda_mpz_bitwise_rshift(t, tmp, RL);
-//
-//	if (cuda_mpz_gte(t , N)){
-//		cuda_mpz_sub(tmp, t, N);
-//		cuda_mpz_set(t, tmp);
-//		return t;
-//    }
-//	else{
-//		cuda_mpz_sub(tmp, t, N);
-//		cuda_mpz_set(tmp, t);
-//	    return t;
-//	}
-//}
+__device__ __host__ cuda_mpz_t* REDC(int RL, cuda_mpz_t* N, cuda_mpz_t* N_, cuda_mpz_t* T, cuda_mpz_t* tmp, cuda_mpz_t* t){//cuda_mpz_t* RMOD, int L, cuda_mpz_t* N, cuda_mpz_t* N_ should not be changed.
+
+	//m = ((T & R) * N_) & R
+	cuda_mpz_bitwise_truncate(t, T, RL);
+	cuda_mpz_mult(tmp, N_, t);
+	cuda_mpz_bitwise_truncate_eq(tmp, RL);
+
+	//t = (T + m*N) >> L
+	cuda_mpz_mult(t, tmp , N);
+	cuda_mpz_add(tmp, T, t);
+	cuda_mpz_bitwise_rshift(t, tmp, RL);
+
+	if (cuda_mpz_gte(t , N)){
+		cuda_mpz_sub(tmp, t, N);
+		cuda_mpz_set(t, tmp);
+		return t;
+    }
+	else{
+		cuda_mpz_sub(tmp, t, N);
+		cuda_mpz_set(tmp, t);
+	    return t;
+	}
+}
 
 __global__ void MontSQMLadder(cuda_mpz_t * mes1, long long unsigned pairs, cuda_mpz_t* _x1, cuda_mpz_t* _x2, cuda_mpz_t* tmp, cuda_mpz_t* tmp2, int rl, cuda_mpz_t r2, cuda_mpz_t vn, cuda_mpz_t vn_, int* eBits, int eLength, long long int* clockTable, cuda_mpz_t* t) {
 
-//	__shared__ digit_t s_index[32];
-//
-//	long long int t1, t2;
-//
-//	int k = blockIdx.x * blockDim.x + threadIdx.x;
-//
-//	//to accelerate the experiment, we put all messages in one kernel launch. In the real case, each message causes one kernel launch.
-//	for(long long unsigned iter1 = 0; iter1 < pairs; iter1++){
-//
-//		cuda_mpz_set(&_x1[k], &mes1[2 * iter1 + k]);//next _x1 access will cause L1 miss if the L1 policy is write evict, same as using mutiple kernels.
-//		s_index[k] = cuda_mpz_get_last_digit(&_x1[k]);//make a dependency to make sure previous store is finished.
-//
-//		t1 = clock64();//beginning of necessary instructions within the kernel
-//
-//		cuda_mpz_t* n = &vn;
-//		cuda_mpz_t* n_ = &vn_;
-//		int j = blockIdx.x * blockDim.x + threadIdx.x;
-//
-//		//_x1 = REDC(rmod,n,n_,mes*r2,l)
-//		cuda_mpz_mult(&tmp2[j], &_x1[j], &r2);
-//		cuda_mpz_set( &_x1[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
-//
-//		//x2 = _x1 * _x1
-//		cuda_mpz_mult(&tmp2[j], &_x1[j], &t[j]);
-//		//_x2 = REDC(rmod,n,n_,_x2,l)
-//		cuda_mpz_set( &_x2[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
-//
-//			if(j == 0){
-//				cuda_mpz_print_str_device(&_x1[j]);
-//				printf(" ");
-//				cuda_mpz_print_str_device(&_x2[j]);
-//				printf("\n");
-//			}
-//
-//		for(int i = eLength - 2; i >= 0; i--){
-//
-//			if(eBits[i] == 0){
-//				//x2 = _x1 * _x2
-//				cuda_mpz_mult(&tmp2[j], &_x1[j], &_x2[j]);
-//				//_x2 = REDC(rmod,n,n_,_x2,l)
-//				cuda_mpz_set( &_x2[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
-//				//_x1 = _x1 * _x1
-//				cuda_mpz_set( &tmp[j], &_x1[j]);
-//				cuda_mpz_mult(&tmp2[j], &_x1[j], &tmp[j]);
-//				//_x1 = REDC(rmod,n,n_,_x1,l)
-//				cuda_mpz_set( &_x1[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
-//			} else {
-//				//_x1 = _x1 * _x2
-//				cuda_mpz_mult(&tmp2[j], &_x1[j], &_x2[j]);
-//				//_x1 = REDC(rmod,n,n_,_x1,l) #changes: more efficient
-//				cuda_mpz_set( &_x1[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
-//				//_x2 = _x2 * _x2
-//				cuda_mpz_set( &tmp[j], &_x2[j]);
-//				cuda_mpz_mult(&tmp2[j], &_x2[j], &tmp[j]);
-//				//_x2 = REDC(rmod,n,n_,_x2,l) #changes: more efficient
-//				cuda_mpz_set( &_x2[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
-//			}
+	__shared__ digit_t s_index[32];
+
+	long long int t1, t2;
+
+	int k = blockIdx.x * blockDim.x + threadIdx.x;
+
+	//to accelerate the experiment, we put all messages in one kernel launch. In the real case, each message causes one kernel launch.
+	for(long long unsigned iter1 = 0; iter1 < pairs; iter1++){
+
+		cuda_mpz_set(&_x1[k], &mes1[2 * iter1 + k]);//next _x1 access will cause L1 miss if the L1 policy is write evict, same as using mutiple kernels.
+		s_index[k] = cuda_mpz_get_last_digit(&_x1[k]);//make a dependency to make sure previous store is finished.
+
+		t1 = clock64();//beginning of necessary instructions within the kernel
+
+		cuda_mpz_t* n = &vn;
+		cuda_mpz_t* n_ = &vn_;
+		int j = blockIdx.x * blockDim.x + threadIdx.x;
+
+		//_x1 = REDC(rmod,n,n_,mes*r2,l)
+		cuda_mpz_mult(&tmp2[j], &_x1[j], &r2);
+		cuda_mpz_set( &_x1[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
+
+		//x2 = _x1 * _x1
+		cuda_mpz_mult(&tmp2[j], &_x1[j], &t[j]);
+		//_x2 = REDC(rmod,n,n_,_x2,l)
+		cuda_mpz_set( &_x2[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
+
+//		if(j == 0){
+//			cuda_mpz_print_str_device(&_x1[j]);
+//			printf(" ");
+//			cuda_mpz_print_str_device(&_x2[j]);
+//			printf("\n");
 //		}
-//
-//		//_x1 = REDC(rmod,n,n_,_x1,l)
-//		cuda_mpz_set( &_x1[j], REDC(rl, n, n_, &_x1[j], &tmp[j], &t[j]) );
-//
-//		s_index[k] = cuda_mpz_get_last_digit(&_x1[k]);//make a dependency to make sure previous store is finished.
-//
-//		t2 = clock64();//end of necessary kernel instructions
-//
-////		printf("combo_num: %lld, iter1: %u, iter2: %u\n", combo_num, iter1, iter2);
-//
-//		if( j == 1){
-//			clockTable[iter1] = t2 - t1;
-//		}
-//	}
+
+		for(int i = eLength - 2; i >= 0; i--){
+
+			if(eBits[i] == 0){
+				//x2 = _x1 * _x2
+				cuda_mpz_mult(&tmp2[j], &_x1[j], &_x2[j]);
+				//_x2 = REDC(rmod,n,n_,_x2,l)
+				cuda_mpz_set( &_x2[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
+				//_x1 = _x1 * _x1
+				cuda_mpz_set( &tmp[j], &_x1[j]);
+				cuda_mpz_mult(&tmp2[j], &_x1[j], &tmp[j]);
+				//_x1 = REDC(rmod,n,n_,_x1,l)
+				cuda_mpz_set( &_x1[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
+			} else {
+				//_x1 = _x1 * _x2
+				cuda_mpz_mult(&tmp2[j], &_x1[j], &_x2[j]);
+				//_x1 = REDC(rmod,n,n_,_x1,l) #changes: more efficient
+				cuda_mpz_set( &_x1[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
+				//_x2 = _x2 * _x2
+				cuda_mpz_set( &tmp[j], &_x2[j]);
+				cuda_mpz_mult(&tmp2[j], &_x2[j], &tmp[j]);
+				//_x2 = REDC(rmod,n,n_,_x2,l) #changes: more efficient
+				cuda_mpz_set( &_x2[j], REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]) );
+			}
+		}
+
+		//_x1 = REDC(rmod,n,n_,_x1,l)
+		cuda_mpz_set( &_x1[j], REDC(rl, n, n_, &_x1[j], &tmp[j], &t[j]) );
+
+		s_index[k] = cuda_mpz_get_last_digit(&_x1[k]);//make a dependency to make sure previous store is finished.
+
+		t2 = clock64();//end of necessary kernel instructions
+
+//		printf("combo_num: %lld, iter1: %u, iter2: %u\n", combo_num, iter1, iter2);
+
+		if( j == 1){
+			clockTable[iter1] = t2 - t1;
+		}
+	}
 }
 
 __global__ void init(cuda_mpz_t* _x1, cuda_mpz_t* _x2, cuda_mpz_t* tmp, cuda_mpz_t* tmp2, cuda_mpz_t* t){
--- complete_recovery/main.cu
+++ complete_recovery/main.cu
@@ -6,35 +6,6 @@
 #include "cuda_mpz.h"
 #include <gmp.h>
 
-cuda_mpz_t* REDC(int RL, cuda_mpz_t* N, cuda_mpz_t* N_, cuda_mpz_t* T, cuda_mpz_t* tmp, cuda_mpz_t* t){//cuda_mpz_t* RMOD, int L, cuda_mpz_t* N, cuda_mpz_t* N_ should not be changed.
-
-	char test_str[1024];
-
-	//m = ((T & R) * N_) & R
-	cuda_mpz_bitwise_truncate(t, T, RL);
-	cuda_mpz_mult(tmp, N_, t);
-	cuda_mpz_bitwise_truncate_eq(tmp, RL);
-
-	printf("%s\n", cuda_mpz_get_str(tmp, test_str, 1024));
-
-	//t = (T + m*N) >> L
-	cuda_mpz_mult(t, tmp , N);
-	cuda_mpz_add(tmp, T, t);
-	cuda_mpz_bitwise_rshift(t, tmp, RL);
-
-	printf("%s\n", cuda_mpz_get_str(t, test_str, 1024));
-
-	if (cuda_mpz_gte(t , N)){
-		cuda_mpz_sub(tmp, t, N);
-		cuda_mpz_set(t, tmp);
-		return t;
-    }
-	else{
-		cuda_mpz_sub(tmp, t, N);
-		cuda_mpz_set(tmp, t);
-	    return t;
-	}
-}
 
 int CheckREDC(int RL, cuda_mpz_t* N, cuda_mpz_t* N_, cuda_mpz_t* T, cuda_mpz_t* tmp, cuda_mpz_t* t){
 
@@ -261,17 +232,17 @@ int CheckDivExp(cuda_mpz_t * mes1, cuda_mpz_t * mes2, int* eBits, int eLength, c
 
 	if( d0_s1_1 != d0_s1_2 || d0_s2_1 != d0_s2_2 ){ //diverge for bit 0
 		if ( d1_s1_1 != d1_s1_2 || d1_s2_1 != d1_s2_2 ){ //diverge for bit 0 and diverge for bit 1
-			printf("debug0\n");
+//			printf("debug0\n");
 			return 0;
 		} else{ //diverge for bit 0 and converge for bit 1
-			printf("debug3\n");
+//			printf("debug3\n");
 			return 3;
 		}
 	} else if ( d1_s1_1 != d1_s1_2 || d1_s2_1 != d1_s2_2 ){ //converge for bit 0, diverge for bit 1
-		printf("debug1\n");
+//		printf("debug1\n");
 		return 1;
 	} else{ //converge for bit 0 and converge for bit 1
-		printf("debug2\n");
+//		printf("debug2\n");
 		return 2;
 	}
 }
@@ -469,20 +440,18 @@ int main (int argc, char *argv[]) {
 		mpz_urandomm (rand_num, rand_state, mod);
 		cuda_mpz_set_gmp(&r2, rand_num);
 
-		char r1_str[] = "0000000030bb981bd55d145233";
-		cuda_mpz_set_str_host(&r1, r1_str);
-		char r2_str[] = "0000000035bd98947ef0b97a5e";
-		cuda_mpz_set_str_host(&r2, r2_str);
-
-		char test_str[1024];
-		printf("%s\n", cuda_mpz_get_str(&r1, test_str, 1024));
-		printf("%s\n", cuda_mpz_get_str(&r2, test_str, 1024));
+//		char r1_str[] = "0000000030bb981bd55d145233";
+//		cuda_mpz_set_str_host(&r1, r1_str);
+//		char r2_str[] = "0000000035bd98947ef0b97a5e";
+//		cuda_mpz_set_str_host(&r2, r2_str);
+//
+//		char test_str[1024];
+//		printf("%s\n", cuda_mpz_get_str(&r1, test_str, 1024));
+//		printf("%s\n", cuda_mpz_get_str(&r2, test_str, 1024));
 
 		div_con = CheckDivExp(&r1, &r2, known_bits, known_bits_length, &_x1_1, &_x1_2, &_x2_1, &_x2_2,
 				&_x1_1_temp, &_x1_2_temp, &_x2_1_temp, &_x2_2_temp,
 				&tmp_1, &tmp_2, &tmp2_1, &tmp2_2, rl, &h_r2, &h_n, &h_n_,  &t_1, &t_2);
-
-		exit(0);
 
 		if (div_con == 1 && bit1_div_num > 0){
 			bit1_div_num--;
@@ -506,6 +475,8 @@ int main (int argc, char *argv[]) {
 			break;
 		}
 	}
+
+	exit(0);
 
 	long long int sum1 = 0;
 	long long int sum2 = 0;
