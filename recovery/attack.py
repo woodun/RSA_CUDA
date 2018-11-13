98f1423d8d7faea6153e5c889714f91c1f78d900
woodun <hwang07@email.wm.edu>
2018-11-13 16:51:48 -0500

    debug

--- recovery/attack.py
+++ recovery/attack.py
@@ -221,7 +221,7 @@ while(eob == 0 ): #32 threads with cache? bothdiv?
     f2 = open("nondivpairs_pre.txt","w+")
     f3 = open("divpairs_pre.txt","w+")
     f4 = open("bit0divpairs_pre.txt","w+")
-    FindPairs (2256, n, current_bits, n_, r2, rmod, l, f1, f2, f3, f4, 1, len(bits(current_bits) ) )
+    FindPairs (2256, n, current_bits, n_, r2, rmod, l, f1, f2, f3, f4, 0, len(bits(current_bits) ) )
     f1.close()
     f2.close()
     f3.close()
@@ -231,7 +231,7 @@ while(eob == 0 ): #32 threads with cache? bothdiv?
     print(sum1)
     sum2 = subprocess.check_output(["./main", "nondivpairs_pre.txt", "2256"])
     print(sum2)
-    sum3 = subprocess.check_output(["./main", "nondivpairs.txt", "2256"])
+    sum3 = subprocess.check_output(["./main", "divpairs_pre.txt", "2256"])
     print(sum3)
     sum4 = subprocess.check_output(["./main", "bit0divpairs_pre.txt", "2256"]) #./main bit0divpairs_pre.txt 1000
     print(sum4)
