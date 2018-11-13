60bdf3cb31141ff02c9178282a7f865bd0c350c3
woodun <hwang07@email.wm.edu>
2018-11-13 09:44:45 -0500

    debug

--- recovery/attack.py
+++ recovery/attack.py
@@ -198,16 +198,16 @@ while(eob == 0 ):
     f1 = open("bit1divpairs_pre.txt","w+",1)
     f2 = open("nondivpairs_pre.txt","w+",1)
     f4 = open("bit0divpairs_pre.txt","w+",1)
-    FindPairs (10000, n, current_bits, n_, r2, rmod, l, f1, f2, f4, 0, len(bits(current_bits) ) )
+    FindPairs (1000, n, current_bits, n_, r2, rmod, l, f1, f2, f4, 1, len(bits(current_bits) ) )
     f1.close()
     f2.close()
     f4.close()
     
-    sum1 = subprocess.check_output(["./main", "bit1divpairs_pre.txt", "10000"])
+    sum1 = subprocess.check_output(["./main", "bit1divpairs_pre.txt", "1000"])
     print(sum1)
-    sum2 = subprocess.check_output(["./main", "nondivpairs_pre.txt", "10000"])
+    sum2 = subprocess.check_output(["./main", "nondivpairs_pre.txt", "1000"])
     print(sum2)
-    sum3 = subprocess.check_output(["./main", "bit0divpairs_pre.txt", "10000"]) #./main bit0divpairs_pre.txt 1000
+    sum3 = subprocess.check_output(["./main", "bit0divpairs_pre.txt", "1000"]) #./main bit0divpairs_pre.txt 1000
     print(sum3)
 
     diff1 = abs( int(sum1) - int(sum2) );
@@ -243,7 +243,7 @@ while(eob == 0 ):
 print("bits: " + bits(current_bits))
 
 
-
+key = "1011011001001001010011110110010101010111001010110101111000111100001"
 
    
 
