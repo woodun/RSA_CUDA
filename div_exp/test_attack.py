d3a62338023aceff69cea1a57b28029c66179a96
woodun <hwang07@email.wm.edu>
2018-11-15 11:13:43 -0500

    debug

--- div_exp/test_attack.py
+++ div_exp/test_attack.py
@@ -233,8 +233,8 @@ while(eob == 0 ):
     f2 = open("nondivpairs_pre.txt","w+")
     f3 = open("divpairs_pre.txt","w+")
     f4 = open("bit0divpairs_pre.txt","w+")
-#     random.seed(1542226726.06)#get a good seed?
-    random.seed(0)#get a good seed?
+#    random.seed(1542226726.06)#get a good seed?
+#    random.seed(0)#get a good seed?
     FindPairs (2256, n, current_bits, n_, r2, rmod, l, f1, f2, f3, f4, int(sys.argv[1]), len(bits(current_bits) ) )
     f1.close()
     f2.close()
