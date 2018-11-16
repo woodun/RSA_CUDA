c97934a029564b9811eb4cf9e9fed91001b3a93c
woodun <hwang07@email.wm.edu>
2018-11-16 00:32:34 -0500

    debug

--- div_exp/kernel.cu
+++ div_exp/kernel.cu
@@ -30,8 +30,8 @@ __device__ int REDC(int RL, mpz_t* N, mpz_t* N_, mpz_t* T, mpz_t* tmp, mpz_t* t)
 __global__ void MontSQMLadder(mpz_t * mes1, long long unsigned pairs, mpz_t* _x1, mpz_t* _x2, mpz_t* tmp, mpz_t* tmp2, int rl, mpz_t r2, mpz_t vn, mpz_t vn_, int* eBits, int eLength, long long int* clockTable, mpz_t* t, int* divTable) {
 
 	__shared__ digit_t s_index[32];
-	__shared__ digit_t con1[2][70];
-	__shared__ digit_t con2[2][70];
+	__shared__ digit_t con1[2][100];
+	__shared__ digit_t con2[2][100];
 
 	long long int t1, t2;
 
@@ -51,13 +51,13 @@ __global__ void MontSQMLadder(mpz_t * mes1, long long unsigned pairs, mpz_t* _x1
 
 		//_x1 = REDC(rmod,n,n_,mes*r2,l)
 		mpz_mult(&tmp2[j], &_x1[j], &r2);
-		con1[j][68] = REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]);
+		con1[j][71] = REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]);
 		mpz_set( &_x1[j], &t[j]);
 
 		//x2 = _x1 * _x1
 		mpz_mult(&tmp2[j], &_x1[j], &t[j]);
 		//_x2 = REDC(rmod,n,n_,_x2,l)
-		con2[j][67] = REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]);
+		con2[j][70] = REDC(rl, n, n_, &tmp2[j], &tmp[j], &t[j]);
 		mpz_set( &_x2[j], &t[j]);
 
 //			if(j == 0){
@@ -67,7 +67,7 @@ __global__ void MontSQMLadder(mpz_t * mes1, long long unsigned pairs, mpz_t* _x1
 //				printf("\n");
 //			}
 
-		for(int i = eLength - 2; i >= 0; i--){ //0 - 65 = 67 - 1
+		for(int i = eLength - 2; i >= 0; i--){ //0 - 65 = 67 - 1 //0 - 68 = 70 - 1
 
 			if(eBits[i] == 0){
 				//x2 = _x1 * _x2
@@ -97,7 +97,7 @@ __global__ void MontSQMLadder(mpz_t * mes1, long long unsigned pairs, mpz_t* _x1
 		}
 
 		//_x1 = REDC(rmod,n,n_,_x1,l)
-		con1[j][66] = REDC(rl, n, n_, &_x1[j], &tmp[j], &t[j]);
+		con1[j][69] = REDC(rl, n, n_, &_x1[j], &tmp[j], &t[j]);
 		mpz_set( &_x1[j], &t[j]);
 
 		s_index[k] = mpz_get_last_digit(&_x1[k]);//make a dependency to make sure previous store is finished.
@@ -108,7 +108,7 @@ __global__ void MontSQMLadder(mpz_t * mes1, long long unsigned pairs, mpz_t* _x1
 			clockTable[iter1] = t2 - t1;
 
 			int div_count = 0;
-			for(int m = 0; m < 69; m++){
+			for(int m = 0; m < 72; m++){
 				if(con1[0][m] != con1[1][m]){
 					div_count++;
 				}
