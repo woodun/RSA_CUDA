330909a8d57972564925457d7131ec6cc4100f64
woodun <hwang07@email.wm.edu>
2018-11-19 13:40:59 -0500

    debug

--- div_exp/debug.py
+++ div_exp/debug.py
@@ -340,7 +340,7 @@ for i in range(1):
     f2 = open("nondivpairs_pre.txt","w+")
     f3 = open("divpairs_pre.txt","w+")
     f4 = open("bit0divpairs_pre.txt","w+")
-    FindPairs (2256, n, current_bits, n_, r2, rmod, l,  f1, f2, f3, f4, 0, len(bits(current_bits) ), d)
+    FindPairs (2000, n, current_bits, n_, r2, rmod, l,  f1, f2, f3, f4, 0, len(bits(current_bits) ), d)
     f1.close()
     f2.close()
     f3.close()
@@ -348,26 +348,26 @@ for i in range(1):
 
     print("#########################################GPU output###########################################")
     #./main bit0divpairs_pre.txt 1000
-    out1 = subprocess.check_output(["./main", "bit1divpairs_pre.txt", "2256", "bit1divpairs_out.txt"]) # greater means 1
+    out1 = subprocess.check_output(["./main", "bit1divpairs_pre.txt", "2000", "bit1divpairs_out.txt"]) # greater means 1
     sum1 = out1.splitlines()[0]
     print(sum1)
     div1 = out1.splitlines()[1]
     print(div1)        
     #print(out1.splitlines()[2])
 
-    out2 = subprocess.check_output(["./main", "nondivpairs_pre.txt", "2256", "nondivpairs_out.txt"])
+    out2 = subprocess.check_output(["./main", "nondivpairs_pre.txt", "2000", "nondivpairs_out.txt"])
     sum2 = out2.splitlines()[0]
     print(sum2)
     div2 = out2.splitlines()[1]
     print(div2)
     
-    out3 = subprocess.check_output(["./main", "divpairs_pre.txt", "2256", "divpairs_out.txt"])
+    out3 = subprocess.check_output(["./main", "divpairs_pre.txt", "2000", "divpairs_out.txt"])
     sum3 = out3.splitlines()[0]
     print(sum3)
     div3 = out3.splitlines()[1]
     print(div3)
     
-    out4 = subprocess.check_output(["./main", "bit0divpairs_pre.txt", "2256", "bit0divpairs_out.txt"]) # greater means 0
+    out4 = subprocess.check_output(["./main", "bit0divpairs_pre.txt", "2000", "bit0divpairs_out.txt"]) # greater means 0
     sum4 = out4.splitlines()[0]
     print(sum4)
     div4 = out4.splitlines()[1]
