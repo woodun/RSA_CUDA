0a336eff969add7d8da989e5c7a733e326610b96
woodun <hwang07@email.wm.edu>
2018-11-19 16:32:24 -0500

    debug

--- div_exp/debug.py
+++ div_exp/debug.py
@@ -447,7 +447,7 @@ for i in range(1):
     print(sum1)
     div1 = out1.splitlines()[1]
     print(div1)        
-    print(out1.splitlines()[2])  
+    #print(out1.splitlines()[2])  
 
     out2 = subprocess.check_output(["./main", "nondivpairs_pre.txt", "1", "nondivpairs_out.txt"])
     sum2 = out2.splitlines()[0]
