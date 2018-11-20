afe193505e064b8c137c41d8c36718378761d21e
woodun <hwang07@email.wm.edu>
2018-11-20 04:55:56 -0500

    debug

--- complete_recovery/main.cu
+++ complete_recovery/main.cu
@@ -375,20 +375,20 @@ int CheckDivExp(cuda_mpz_t * mes1, cuda_mpz_t * mes2, int* eBits, int eLength, c
 
 	if ( (d0_s1_1 != d0_s1_2 && d0_s2_1 == d0_s2_2) || (d0_s1_1 == d0_s1_2 && d0_s2_1 != d0_s2_2) ){ //diverge for bit 0 (1 0) or (0 1)
 		if ( (d1_s1_1 != d1_s1_2 && d1_s2_1 == d1_s2_2) or (d1_s1_1 == d1_s1_2 && d1_s2_1 != d1_s2_2) ){ //diverge for bit 0, diverge for bit 1 (1 0) or (0 1)
-			printf ("debug3\n")
+			printf ("debug3\n");
 			return 3;
 		} else if ( d1_s1_1 == d1_s1_2 && d1_s2_1 == d1_s2_2 ) { //diverge for bit 0, converge for bit 1 (0 0)
-			prinft ("debug4\n")
+			printf ("debug4\n");
 			return 4;
 		} else {
 			return 0;
 		}
 	} else if (d0_s1_1 == d0_s1_2 && d0_s2_1 == d0_s2_2 ){ //converge for bit 0 (0 0)
 		if ( (d1_s1_1 != d1_s1_2 && d1_s2_1 == d1_s2_2) || (d1_s1_1 == d1_s1_2 && d1_s2_1 != d1_s2_2) ){ //converge for bit 0, diverge for bit 1 (1 0) or (0 1)
-			printf ("debug1\n")
+			printf ("debug1\n");
 			return 1;
 		} else if ( d1_s1_1 == d1_s1_2 && d1_s2_1 == d1_s2_2 ){ //converge for bit 0, converge for bit 1 (0 0)
-			printf ("debug2\n")
+			printf ("debug2\n");
 			return 2;
 		} else {
 			return 0;
